eff2507f8ca63ce1748e765cd52a617b1353482b
elyes95 <elyes.aitsaid@gmail.com>
2024-04-11 17:33:48 +0200

    ✏️ F01:LOGIN>Form>Fix typo

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -23,7 +23,7 @@ export default function LoginForm() {
     //affichage (render)
     return (
         <form action="submit" onSubmit={handleSubmit}>
-            <h1>Bienvenue chez nous!</h1>
+            <h1>Bienvenue chez nous !</h1>
             <br />
             <h2>Connectez-vous</h2>
             <input type="text"
