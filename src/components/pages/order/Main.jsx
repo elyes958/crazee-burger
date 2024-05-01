004e257d58cd946dcd67effecb95543fc835d303
elyes95 <elyes.aitsaid@gmail.com>
2024-05-01 17:03:27 +0200

    💄 F05: Travail effectuer avant la correction

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -1,9 +1,35 @@
 import styled from "styled-components";
 import { theme } from "../../../theme";
+import { useState } from "react";
+import { fakeMenu2 } from "../../../fakeData/fakeMenu";
 
 export default function Main() {
+  // State
+  const [data, setData] = useState(fakeMenu2);
+
+  // Comportements
+  const affichage = data.map((element) => {
+    return(
+      <div className="carte">
+        <img src={element.imageSource} alt="image" />
+        <div>
+          <h3>{element.title}</h3>
+          <div className="priceButton">
+            <div>
+                <span>{element.price.toFixed(2)} €</span> 
+            </div>
+            <button>Ajouter</button>
+          </div>
+        </div>
+      </div>
+    )
+  });
+
+
+  // Affichage
   return (
     <MainStyled>
+      {affichage}
     </MainStyled>
   )
 }
@@ -16,4 +42,65 @@ const MainStyled = styled.main`
     border-bottom-right-radius: ${theme.borderRadius.extraRound};
     // ont aurait pu egalement juste coller le border radius du ticket 0 0 15 15 = en bas a droite et a gauche pour les 2 15px
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
+    display: grid;
+    grid-template-columns: repeat(4, 240px);
+    grid-column-gap: 85px;
+    grid-template-rows: repeat(4, 330px);
+    grid-row-gap: 60px;
+    padding: 50px 50px 150px;  // avec le padding ça marche, le margin que j'ai mis sur les éléments provoquer des erreurs ça ne fonctionner pas
+    
+   h3{
+    font-family: "Amatic SC";
+    font-weight: ${theme.fonts.weights.bold};
+    font-size: ${theme.fonts.size.P4};
+    margin: 20px 20px 0px 20px;
+   }
+
+   div img {
+    width: 200px;
+    height: 145px;
+    object-fit: cover; /* Empêche l'image de se déformer et la fait remplir le conteneur */
+    margin: 50px 20px 0px 20px;
+   }
+
+   .carte{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    /* align-items: center; */
+    box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
+    border-radius: ${theme.borderRadius.extraRound};
+   }
+
+  .priceButton{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  .priceButton span{
+    font-weight: ${theme.fonts.weights.regular};
+    font-size: ${theme.fonts.size.P0};
+    color: ${theme.colors.primary};
+  }
+
+  .priceButton div{
+    padding: 22px, 50px, 22px, 0px;
+  }
+
+  .priceButton button{
+    background: ${theme.colors.primary};
+    color: ${theme.colors.white};
+    outline: none; // sert a enlever la bordure par default qui apparai au clic
+    transition: background 0.3s ease; // effet de transition progresive du changement de background
+    &:hover{   // = au survol
+      background: ${theme.colors.white};
+      color: ${theme.colors.primary};
+      border: 1px solid ${theme.colors.primary};
+    }
+    &:active{  // = au clic
+      background: ${theme.colors.primary};
+      color: ${theme.colors.white};
+    }
+  }
 `;
--- src/components/pages/order/Navbar.jsx
+++ src/components/pages/order/Navbar.jsx
@@ -22,6 +22,9 @@ const NavbarStyled = styled.nav`
     padding: 0 20px;   // pour gerer l'espace des element à l'interieur de la nav(comme expliquer ont fait ça avec le parent pour ne pas modifier le composant reutilisable voir maquette figma il y'a 20px d'espace entre logo et le debut de nav et l'autre div a un peu plus)
     border-top-left-radius: ${theme.borderRadius.extraRound};  // uniquement le coin haut gauche
     border-top-right-radius: ${theme.borderRadius.extraRound};
+    position: sticky;      // En utilisant position: sticky sur votre barre de navigation et en ajoutant une marge supérieure à votre conteneur principal, vos cartes passeront en dessous de la barre de navigation lors du défilement.
+    top: 0;               /* Coller ma nav en haut de la fenêtre du navigateur */
+    z-index: 2;           /* Assurez-vous que la barre de navigation reste au-dessus du reste du contenu la j'ai mis 2 mais tu peu mettre plus si besoin */
     /* align-items: center; */
     .logo-order-page{
         cursor: pointer;
