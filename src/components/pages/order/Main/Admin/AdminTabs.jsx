286d033556a14b56ea1f6688f89d8017cb7be340
elyes95 <elyes.aitsaid@gmail.com>
2024-05-20 16:59:55 +0200

    💄 F07: ADMIN > Tabs > Add Tab

--- src/components/pages/order/Main/Admin/AdminTabs.jsx
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -1,11 +1,17 @@
 import styled from "styled-components";
+import Tab from "../../../../reusable-ui/Tab.jsx";
+import { FiChevronDown } from "react-icons/fi";
 
 export default function AdminTabs() {
   return (
-    <AdminTabsStyled>AdminTabs</AdminTabsStyled>
+    <AdminTabsStyled>
+        <Tab Icon={<FiChevronDown/>} />
+    </AdminTabsStyled>
   )
 }
 
-const AdminTabsStyled = styled.div`
-  border: 1px solid red;
+const AdminTabsStyled = styled.button`
+  /* border: 1px solid red; */
+  display: flex;
+  padding: 0 20px;
 `;
